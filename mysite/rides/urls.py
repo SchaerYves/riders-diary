8eb5903befe952902ce9c46e959817603d90d6a0
Yves <yves.schaer@gmail.com>
2022-07-24 14:57:41 +0200

    django first app part 4

--- mysite/rides/urls.py
+++ mysite/rides/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = 'rides'
 urlpatterns = [
+    # ex: /rides/
     path('', views.index, name='index'),
+    # ex: /rides/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /rides/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /rides/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
